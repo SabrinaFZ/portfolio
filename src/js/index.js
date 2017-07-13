efbb85e5d45b58b271fb917ae39890344923fcd2
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-07-13 21:49:45 +0200

    Modify index

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+
+//Styles
+require('react-bootstrap')
+import '../styles/index.scss'
+import '../styles/colors.scss'
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+
+  //entry point-> requires other modules (root)
+  entry: {
+    app:'./src/js/index.js'
+  },
+
+  //ouput point->
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].js'
+  },
+
+  module:{
+    loaders:[
+      {
+        test:/\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query:{
+          presets:['react','es2015']
+        }
+      },
+      {
+        test:/\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      },
+      {
+        test:/\.pug$/,
+        loader: 'html-loader!pug-html-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Portfolio',
+      template: 'src/index.pug'
+    })
+  ]
+
+}
