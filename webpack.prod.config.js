18d71b16498fd530e8feb7ce9379270b24df94b0
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-07-16 20:46:27 +0200

    fix prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js',
-    publicPath: ' https://portfolio-sabrina.herokuapp.com/'
+    publicPath: '/dist/'
   },
 
   plugins: [
