dc1a63109c9bc66d6fb8e280d9d18c4483373d0d
SabrinaFZ <sabrina.fer.zam@gmail.com>
2018-12-24 14:24:33 +0100

    configure webpack 4 in portfolio

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import colors from './styles/colors.scss';
+import fonts from './styles/fonts.scss';
+import style from './styles/style.scss';
+
+import favicon from './assets/img/favicon.ico';
+import profile from './assets/img/pic.jpg';
+import github from './assets/img/icons/github.svg';
+import linkedin from './assets/img/icons/linkedin.svg';
+import mail from './assets/img/icons/mail.svg';
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    },
+                    {
+                        loader: 'pug-html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true}
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: false }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|ico|jpg|gif)(\?.*)?$/,
+                loader: 'file-loader',
+                query: {
+                    name: 'assets/img/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'file-loader',
+                query: {
+                    name: 'assets/fonts/[name].[ext]'
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.pug',
+            filename: './index.html',
+            title: 'Portfolio'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
