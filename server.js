a7fe81f8ae37726b1a47b127e9a595f1c9cf779f
SabrinaFZ <sabrina.fer.zam@gmail.com>
2018-12-31 19:13:40 +0100

    fix server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(publicPath);
 
 // routes
 app.get('/', (req, res)=> {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
 // start server
