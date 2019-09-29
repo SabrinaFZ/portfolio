926a74d8d2f61ae4f7ba09886b8bffe77d3411e4
SabrinaFZ <sabrina.fer.zam@gmail.com>
2019-09-29 19:17:48 +0200

    Delete icons

--- src/index.js
+++ src/index.js
@@ -12,12 +12,6 @@ import profile from './assets/img/personal_pic.jpg';
 import github from './assets/img/icons/github.svg';
 import linkedin from './assets/img/icons/linkedin.svg';
 import mail from './assets/img/icons/mail.svg';
-import bfb from './assets/img/icons/bfb.jpg';
-import amaris from './assets/img/icons/amaris.png';
-import keepcoding from './assets/img/icons/kc.png';
-import upm from './assets/img/icons/upm.png';
-import udemy from './assets/img/icons/udemy.jpg';
-import miriadax from './assets/img/icons/miriadax.png';
 
 //js
 import navigator from './js/navigator.js';
