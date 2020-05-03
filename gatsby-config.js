4971548022aad9ad20b8498e8cf9c3ad234aaf99
SabrinaFZ <sabrina.fer.zam@gmail.com>
2020-05-03 22:23:05 +0200

    Create deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/portfolio",
   siteMetadata: {
     title: `Sabrina's Portfolio`,
     description: `Personal website with information, projects and contact details.`,
