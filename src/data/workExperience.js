0a90d21e3ca87b6dff7fcebdcd7fab417f421d26
SabrinaFZ <sabrina.fer.zam@gmail.com>
2020-05-24 13:53:36 +0200

    Modify work experience

--- src/data/workExperience.js
+++ src/data/workExperience.js
@@ -1,6 +1,19 @@
 export default [
   {
     id: 'work-experience',
+    date: '04/2020 - Present',
+    company: 'Con&Sel15',
+    role: 'Front-end Developer',
+    location: 'Madrid, Spain',
+    tasks: [
+      'Resposible for new developments using technologies such as '
+      + 'Vue, Vuex, CSS, HTML, Javascript, Sass, '
+      + 'Git, Azure',
+      'Application maintenance'
+    ]
+  },
+  {
+    id: 'work-experience',
     date: '01/2018 - 04/2020',
     company: 'Amaris',
     role: 'Front-end Developer',
