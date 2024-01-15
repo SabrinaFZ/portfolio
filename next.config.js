cea5acb088d70cc2be7335231b0dfc5f3a4ddf8e
Sabrina Fernandez Zambrano <sabrina.fer.zam@gmail.com>
2024-01-15 19:29:08 +0100

    refactor: modify deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.BASE_PATH,
 }
 
 module.exports = nextConfig
