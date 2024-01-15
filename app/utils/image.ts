b84c181cfd287b1fdbe8ed06b774696a953db081
Sabrina Fernandez Zambrano <sabrina.fer.zam@gmail.com>
2024-01-15 22:31:26 +0100

    feat:debug

--- app/utils/image.ts
+++ app/utils/image.ts
@@ -1,4 +1,5 @@
 export function buildImageUrl(src: string) {
-  console.log('env: ', process.env.NODE_ENV)
+  console.log('env: ', process.env.NODE_ENV === 'production')
+  console.log('prefiix', process.env.NEXT_PUBLIC_ASSET_PREFIX)
   return process.env.NODE_ENV === 'production' ? `${process.env.NEXT_PUBLIC_ASSET_PREFIX}${src}` : `${src}`;
 };
