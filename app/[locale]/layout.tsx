6c7d077f78ca520d3638707b3a7428cc7f0bf691
Sabrina Elena Fernandez <sabrina.fer.zam@gmail.com>
2024-01-16 17:14:08 +0100

    feat: install i18n

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Portfolio",
+    default: "Home | Portfolio",
+  },
+  description: "Sabrina Fernandez Zambrano portfolio",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html className="scroll-smooth">
+      <body className={`${inter.className} bg-slate-800 text-white pb-12`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/[locale]/page.tsx
@@ -0,0 +1,11 @@
+import Content from "../ui/content";
+import { Header } from "../ui/header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Content />
+    </main>
+  );
+}
