4d8ba156bf87b4d6fc5f44d82981073dba0bca17
Sabrina Elena Fernandez <sabrina.fer.zam@gmail.com>
2024-01-18 10:41:09 +0100

    fix: arrow position

--- app/ui/header/arrow.tsx
+++ app/ui/header/arrow.tsx
@@ -4,7 +4,7 @@ export default function Arrow() {
   return (
     <Link
       href="#content"
-      className="absolute bottom-10"
+      className="absolute bottom-5"
       aria-label="main content"
     >
       <div className="w-0 h-0 border-x-8 border-x-transparent border-t-[16px] border-white cursor-pointer animate-bounce"></div>
